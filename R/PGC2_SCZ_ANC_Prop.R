16374075efde5062dc4731265686205156fc55da
Donghyung Lee <dleelab@users.noreply.github.com>
2023-07-31 05:01:10 -0400

    pgc2 scz ancestry proportion data r file added

---
+++ R/PGC2_SCZ_ANC_Prop.R
@@ -0,0 +1,27 @@
+#' PGC2 Schizophrenia GWAS Ancestry Proportion
+#'
+#' This data set include two columns with the following names: 
+#' pop (population abbreviation) wgt (weight or proportion). This is estimated 
+#' using the `afmix()` function.
+#'
+#' @docType data
+#'
+#' @format An object of class 'data.frame' with 2 columns.
+#'
+#' @usage data(PGC2_SCZ_ANC_Prop)
+#'
+#' @keywords datasets
+#'
+#' @references
+#' Schizophrenia Working Group of Psychiatric Genomics Consortium (2014). 
+#' Biological insights from 108 schizophrenia associated genetic loci. Nature
+#' \href{https://www.nature.com/articles/nature13595}{Link to article}
+#'
+#' @examples
+#' \dontrun{
+#' data(PGC2_SCZ_ANC_Prop)
+#' str(PGC2_SCZ_ANC_Prop)
+#' }
+#'
+#' @name PGC2_SCZ_ANC_Prop
+NULL
